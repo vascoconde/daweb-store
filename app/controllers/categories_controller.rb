f91b3dc3d0dab415f723178fb7a04c1ce3aa7b14
Vasco Conde <vasco@elasticfiction.com>
2012-12-10 20:46:12 +0000

    Now links to categories work! And show products in that category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,19 +2,16 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @categories }
-    end
+  	redirect_to root_path
   end
 
   # GET /categories/1
   # GET /categories/1.json
   def show
+  	@categories =Category.all
     @category = Category.find(params[:id])
-
+	@products = @category.products
+	
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @category }
