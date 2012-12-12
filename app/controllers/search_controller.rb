8c401047cb6eb675aa87a4d5178dbb7eadd13576
Vasco Conde <vasco@elasticfiction.com>
2012-12-12 22:30:13 +0000

    Search is working! Now it searches only in the name of the product... Can be improved.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,8 @@ class SearchController < ApplicationController
  def index
  	@categories = Category.all
  	@search_query = params[:search]
- end
- 
- def show
- 
- end
+ 	@products = Product.where("name LIKE ?", "%#{@search_query}%")
 
+ end
 
 end
