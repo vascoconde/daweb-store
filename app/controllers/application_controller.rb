91215d749fb15c67102f381b75fc0c1ef9b8029d
miguel <miguelj_5@hotmail.com>
2012-12-19 14:22:19 +0000

    Added link to product from home page; Changed description size

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,11 @@ class ApplicationController < ActionController::Base
 private
   
   def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id]) rescue nil
+    end
   end
+
   helper_method :current_user
   
   def current_cart
