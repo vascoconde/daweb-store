333738871dbc8bcee63143955a0892a7bc09fda4
Vasco Conde <vasco@elasticfiction.com>
2012-12-22 17:03:41 +0000

    Add user admin to seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :orders
 
-  attr_accessible :email, :password, :username, :cart, :orders
+  attr_accessible :email, :password, :username, :cart, :orders, :admin
   
   validates :email, :password, :username, :presence => true
+  
 end
